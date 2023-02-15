0bfe2d476198f000a58de4c5d95ad38f2eac7b9b
golu7679 <golurajak811@gmail.com>
2023-02-15 17:56:16 +0530

    New: helmet added Update: dependencies updated Update: security middleware added in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: console,
+
+  });
+
+  // global middleware and security checkup
+  app.disable('x-powered-by');
+  app.enableCors();
   app.enableVersioning();
   app.enableShutdownHooks();
+
+  app.use(helmet());
+  app.use(helmet.noSniff());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.contentSecurityPolicy());
+
+  await app.listen(3000);
+
 }
 bootstrap();
